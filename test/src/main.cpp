de073e04754d839ee1b82baab7ad2dd91b4f30e8
lxrobotics <cto@lxrobotics.com>
2018-12-18 11:29:41 +0100

    Implementing test and production code for encoding float properties for both protocols

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -107,4 +107,38 @@ SCENARIO("Arduino Cloud Properties are encoded", "[ArduinoCloudThing::encode]")
   }
 
   /************************************************************************************/
+
+  WHEN("A 'float' property is added")
+  {
+    GIVEN("CloudProtocol::V1")
+    {
+      ArduinoCloudThing thing(CloudProtocol::V1);
+      thing.begin();
+      encode(thing);
+
+      float float_test = 3.14159;
+      thing.addPropertyReal(float_test, "test", Permission::ReadWrite);
+
+      /* [{"n": "test", "v": 3.141590118408203}] = 81 BF 61 6E 64 74 65 73 74 61 76 FA 40 49 0F D0 FF */
+      std::vector<uint8_t> const expected = {0x81, 0xBF, 0x61, 0x6E, 0x64, 0x74, 0x65, 0x73, 0x74, 0x61, 0x76, 0xFA, 0x40, 0x49, 0x0F, 0xD0, 0xFF};
+      std::vector<uint8_t> const actual = encode(thing);
+      REQUIRE(actual == expected);
+    }
+    GIVEN("CloudProtocol::V2")
+    {
+      ArduinoCloudThing thing(CloudProtocol::V2);
+      thing.begin();
+      encode(thing);
+
+      float float_test = 3.14159;
+      thing.addPropertyReal(float_test, "test", Permission::ReadWrite);
+
+      /* [{0: "test", 2: 3.141590118408203}] = 81 BF 00 64 74 65 73 74 02 FA 40 49 0F D0 FF */
+      std::vector<uint8_t> const expected = {0x81, 0xBF, 0x00, 0x64, 0x74, 0x65, 0x73, 0x74, 0x02, 0xFA, 0x40, 0x49, 0x0F, 0xD0, 0xFF};
+      std::vector<uint8_t> const actual = encode(thing);
+      REQUIRE(actual == expected);
+    }
+  }
+
+  /************************************************************************************/
 }
