66568b1ed4153fa5ceaab5afd9c0e6a2537dcaca
lxrobotics <cto@lxrobotics.com>
2018-12-18 07:27:27 +0100

    Adding first draft of how we could test the different protocol versions

--- test/main.cpp
+++ test/main.cpp
@@ -18,23 +18,48 @@
 #include <catch.hpp>
 
 /**************************************************************************************
+ * TEST HELPER FUNCTIONS
+ **************************************************************************************/
+
+std::vector<uint8_t> encode(ArduinoCloudThing & thing)
+{
+  uint8_t buf[200] = {0};
+  int const bytes_buf = thing.encode(buf, 200);
+  return std::vector<uint8_t>(buf, buf + bytes_buf);
+}
+
+/**************************************************************************************
  * TEST CODE
  **************************************************************************************/
 
 SCENARIO("Arduino Cloud Properties are encoded", "[ArduinoCloudThing::encode]")
 {
-  GIVEN("No properties besides the default 'status' property have been added")
+  GIVEN("Cloud Protocol V1")
   {
-    ArduinoCloudThing thing;
+    ArduinoCloudThing thing(CloudProtocol::V1);
     thing.begin();
 
-    uint8_t actual[200] = {0};
-    int const bytes_actual = thing.encode(actual, 200);
-    std::vector<uint8_t> const actual_vect(actual, actual + bytes_actual);
-
-   /* http://cbor.me    [{0: "status", 4: true}] = 81 BF 00 66 73 74 61 74 75 73 04 F5 FF */
-    std::vector<uint8_t> const expected_vect = {0x81, 0xBF, 0x00, 0x66, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x04, 0xF5, 0xFF};
-
-    REQUIRE(actual_vect == expected_vect);
+    WHEN("No properties besides the default 'status' property have been added")
+    {
+      std::vector<uint8_t> const actual   = encode(thing);
+      /* [{"n": "test", "vb": true}] = 81 A2 61 6E 64 74 65 73 74 62 76 62 F5 */
+      std::vector<uint8_t> const expected = {0x81, 0xA2, 0x61, 0x6E, 0x64, 0x74, 0x65, 0x73, 0x74, 0x62, 0x76, 0x62, 0xF5};
+      REQUIRE(actual == expected);
+    }
   }
+  /************************************************************************************/
+  GIVEN("Cloud Protocol V2")
+  {
+    ArduinoCloudThing thing(CloudProtocol::V2);
+    thing.begin();
+
+    WHEN("No properties besides the default 'status' property have been added")
+    {
+      std::vector<uint8_t> const actual   = encode(thing);
+      /* [{0: "test", 4: true}] = 81 BF 00 64 74 65 73 74 04 F5 FF */
+      std::vector<uint8_t> const expected = {0x81, 0xBF, 0x00, 0x66, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x04, 0xF5, 0xFF};
+      REQUIRE(actual == expected);
+    }
+  }
+
 }
