21c8d3495a834b39f427716540c266895758b0c4
lxrobotics <cto@lxrobotics.com>
2018-12-18 10:34:34 +0100

    Support for CloudProtocol::V1/V2 implemented for boolean values

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -35,32 +35,30 @@ std::vector<uint8_t> encode(ArduinoCloudThing & thing)
 SCENARIO("Arduino Cloud Properties are encoded", "[ArduinoCloudThing::encode]")
 {
   /************************************************************************************/
-  GIVEN("Cloud Protocol V1")
+  WHEN("Only the boolean 'status' default property has been added")
   {
-    ArduinoCloudThing thing(CloudProtocol::V1);
-    thing.begin();
-
-    WHEN("No properties besides the default 'status' property have been added")
+    /**********************************************************************************/
+    GIVEN("CloudProtocol::V1")
     {
-      std::vector<uint8_t> const actual   = encode(thing);
-      /* [{"n": "test", "vb": true}] = 81 A2 61 6E 64 74 65 73 74 62 76 62 F5 */
-      std::vector<uint8_t> const expected = {0x81, 0xA2, 0x61, 0x6E, 0x64, 0x74, 0x65, 0x73, 0x74, 0x62, 0x76, 0x62, 0xF5};
+      ArduinoCloudThing thing(CloudProtocol::V1);
+      thing.begin();
+
+      std::vector<uint8_t> const actual = encode(thing);
+      /* [{"n": "test", "vb": true}] = 81 BF 61 6E 66 73 74 61 74 75 73 62 76 62 F5 FF */
+      std::vector<uint8_t> const expected = {0x81, 0xBF, 0x61, 0x6E, 0x66, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x62, 0x76, 0x62, 0xF5, 0xFF};
       REQUIRE(actual == expected);
     }
-  }
-  /************************************************************************************/
-  GIVEN("Cloud Protocol V2")
-  {
-    ArduinoCloudThing thing(CloudProtocol::V2);
-    thing.begin();
-
-    WHEN("No properties besides the default 'status' property have been added")
+    /**********************************************************************************/
+    GIVEN("CloudProtocol::V2")
     {
-      std::vector<uint8_t> const actual   = encode(thing);
+      ArduinoCloudThing thing(CloudProtocol::V2);
+      thing.begin();
+
+      std::vector<uint8_t> const actual = encode(thing);
       /* [{0: "test", 4: true}] = 81 BF 00 64 74 65 73 74 04 F5 FF */
       std::vector<uint8_t> const expected = {0x81, 0xBF, 0x00, 0x66, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x04, 0xF5, 0xFF};
       REQUIRE(actual == expected);
     }
   }
-
+  /************************************************************************************/
 }
