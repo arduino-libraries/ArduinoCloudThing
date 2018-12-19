f35ff5948f8a67ceef9bd522fd48e302c76dd2ec
lxrobotics <cto@lxrobotics.com>
2018-12-19 13:54:09 +0100

    Fixing wrong comment

--- test/src/test_encode.cpp
+++ test/src/test_encode.cpp
@@ -11,7 +11,7 @@ SCENARIO("Arduino Cloud Properties are encoded", "[ArduinoCloudThing::encode]")
       ArduinoCloudThing thing(CloudProtocol::V1);
       thing.begin();
 
-      /* [{"n": "test", "vb": true}] = 81 BF 61 6E 66 73 74 61 74 75 73 62 76 62 F5 FF */
+      /* [{"n": "status", "vb": true}] = 81 BF 61 6E 66 73 74 61 74 75 73 62 76 62 F5 FF */
       std::vector<uint8_t> const expected = {0x81, 0xBF, 0x61, 0x6E, 0x66, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x62, 0x76, 0x62, 0xF5, 0xFF};
       std::vector<uint8_t> const actual = encode(thing);
       REQUIRE(actual == expected);
@@ -21,7 +21,7 @@ SCENARIO("Arduino Cloud Properties are encoded", "[ArduinoCloudThing::encode]")
       ArduinoCloudThing thing(CloudProtocol::V2);
       thing.begin();
 
-      /* [{0: "test", 4: true}] = 81 BF 00 64 74 65 73 74 04 F5 FF */
+      /* [{0: "status", 4: true}] = 81 BF 00 66 73 74 61 74 75 73 04 F5 FF */
       std::vector<uint8_t> const expected = {0x81, 0xBF, 0x00, 0x66, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x04, 0xF5, 0xFF};
       std::vector<uint8_t> const actual = encode(thing);
       REQUIRE(actual == expected);
