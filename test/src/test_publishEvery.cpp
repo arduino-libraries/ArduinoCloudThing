5a7ec913e115f5f51354d7e2443716f57fc8cc5b
lxrobotics <cto@lxrobotics.com>
2019-01-08 15:06:19 +0100

    Fixing bug in test case - test was not sufficiently expressive

--- test/src/test_publishEvery.cpp
+++ test/src/test_publishEvery.cpp
@@ -18,18 +18,29 @@ SCENARIO("A Arduino cloud property is published periodically", "[ArduinoCloudThi
     thing.addPropertyReal(test, "test", Permission::ReadWrite).publishEvery(PUBLISH_INTERVAL_SEC);
     REQUIRE(encode(thing).size() != 0); /* Initial encoding of the 'test' property */
 
+    bool test_property_published = false;
+
     unsigned long start = millis();
-    for(int i = 0; i < 3; i++) {
+    for(size_t i = 0; i < 5; )
+    {
       /* If we have not reached the publish interval yet - no data should be generated in 'encode' */
-      if((millis() - start) < (PUBLISH_INTERVAL_SEC * 1000)) {
+      if((millis() - start) < (PUBLISH_INTERVAL_SEC * 1000))
+      {
         REQUIRE(encode(thing).size() == 0); /* 'test' property should not be encoded */
       }
-      /* If we have reached the publish interval - no data should be generated in 'encode' */
-      else {
-        REQUIRE(encode(thing).size() != 0); /* Encoding the 'test' property */
-        start = millis();
+      /* If we have reached the publish interval - data should be generated in 'encode' */
+      else
+      {
+        test_property_published = encode(thing).size() != 0; /* Encoding the 'test' property */
+        REQUIRE(test_property_published == true);
+        if(test_property_published)
+        {
+          i++;
+          test_property_published = false;
+          start = millis();
+        }
       }
-      delay(1000);
+      delay(10); /* Delay 10 ms */
     }
   }
   GIVEN("CloudProtocol::V2")
