7e28b7ae9b5038020d6b02fbbb13846e30721bda
lxrobotics <cto@lxrobotics.com>
2019-01-23 08:37:47 +0100

    Fixing bug when decoding multiple strings

--- test/src/test_decode.cpp
+++ test/src/test_decode.cpp
@@ -17,7 +17,6 @@ SCENARIO("Arduino Cloud Properties are decoded", "[ArduinoCloudThing::decode]")
       thing.addPropertyReal(test, "test", Permission::ReadWrite);
 
       /* [{"n": "test", "vb": false}] = 81 A2 61 6E 64 74 65 73 74 62 76 62 F4 */
-
       uint8_t const payload[] = {0x81, 0xA2, 0x61, 0x6E, 0x64, 0x74, 0x65, 0x73, 0x74, 0x62, 0x76, 0x62, 0xF4};
       int const payload_length = sizeof(payload)/sizeof(uint8_t);
       thing.decode(payload, payload_length);
@@ -45,7 +44,6 @@ SCENARIO("Arduino Cloud Properties are decoded", "[ArduinoCloudThing::decode]")
       thing.addPropertyReal(test, "test", Permission::ReadWrite);
 
       /* [{"n": "test", "v": 7}] = 81 A2 61 6E 64 74 65 73 74 61 76 07 */
-
       uint8_t const payload[] = {0x81, 0xA2, 0x61, 0x6E, 0x64, 0x74, 0x65, 0x73, 0x74, 0x61, 0x76, 0x07};
       int const payload_length = sizeof(payload)/sizeof(uint8_t);
       thing.decode(payload, payload_length);
@@ -73,7 +71,6 @@ SCENARIO("Arduino Cloud Properties are decoded", "[ArduinoCloudThing::decode]")
       thing.addPropertyReal(test, "test", Permission::ReadWrite);
 
       /* [{"n": "test", "v": 3.1459}] = 81 A2 61 6E 64 74 65 73 74 61 76 FB 40 09 2A CD 9E 83 E4 26 */
-
       uint8_t const payload[] = {0x81, 0xA2, 0x61, 0x6E, 0x64, 0x74, 0x65, 0x73, 0x74, 0x61, 0x76, 0xFB, 0x40, 0x09, 0x2A, 0xCD, 0x9E, 0x83, 0xE4, 0x26};
       int const payload_length = sizeof(payload)/sizeof(uint8_t);
       thing.decode(payload, payload_length);
