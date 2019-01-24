1ab9c3b29b8622ec2d01fc5e15dedd592457f845
lxrobotics <cto@lxrobotics.com>
2019-01-24 09:23:17 +0100

    Adding test logic for testing if callbacks are called when using CloudProtocol::V2

--- test/src/test_callback.cpp
+++ test/src/test_callback.cpp
@@ -27,7 +27,7 @@ void externalCallbackV2()
  * TEST CODE
  **************************************************************************************/
 
-SCENARIO("A callback is registered to be called on property change", "[ArduinoCloudThing::decode]")
+SCENARIO("A callback is registered via 'onUpdate' to be called on property change", "[ArduinoCloudThing::decode]")
 {
   /************************************************************************************/
 
@@ -39,9 +39,8 @@ SCENARIO("A callback is registered to be called on property change", "[ArduinoCl
     int test = 10;
     thing.addPropertyReal(test, "test", Permission::ReadWrite).onUpdate(externalCallbackV1);
 
-    /* [{"n": "test", "v": 6}] = 9F BF 61 6E 64 74 65 73 74 61 76 6 FF FF */
-
-    uint8_t const payload[] = {0x9F, 0xBF, 0x61, 0x6E, 0x64, 0x74, 0x65, 0x73, 0x74, 0x61, 0x76, 0x6, 0xFF, 0xFF};
+    /* [{"n": "test", "v": 7}] = 81 A2 61 6E 64 74 65 73 74 61 76 07 */
+    uint8_t const payload[] = {0x81, 0xA2, 0x61, 0x6E, 0x64, 0x74, 0x65, 0x73, 0x74, 0x61, 0x76, 0x07};
     int const payload_length = sizeof(payload)/sizeof(uint8_t);
     thing.decode(payload, payload_length);
 
@@ -49,7 +48,18 @@ SCENARIO("A callback is registered to be called on property change", "[ArduinoCl
   }
   GIVEN("CloudProtocol::V2")
   {
-    /* TODO */
+    ArduinoCloudThing thing(CloudProtocol::V2);
+    thing.begin();
+
+    int test = 10;
+    thing.addPropertyReal(test, "test", Permission::ReadWrite).onUpdate(externalCallbackV2);
+
+    /* [{0: "test", 2: 7}] = 81 A2 00 64 74 65 73 74 02 07 */
+    uint8_t const payload[] = {0x81, 0xA2, 0x00, 0x64, 0x74, 0x65, 0x73, 0x74, 0x02, 0x07};
+    int const payload_length = sizeof(payload)/sizeof(uint8_t);
+    thing.decode(payload, payload_length);
+
+    REQUIRE(callback_called_protocol_v2 == true);
   }
 
   /************************************************************************************/
