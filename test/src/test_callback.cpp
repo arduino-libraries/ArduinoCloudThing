fe13c5df509c8d3fd86cee982075a796c394edc3
lxrobotics <cto@lxrobotics.com>
2019-02-05 18:53:25 +0100

    Bugfix + test code - if a property is manipulated in its onChange callback function to its origin state the change is still propagated to the cloud

--- test/src/test_callback.cpp
+++ test/src/test_callback.cpp
@@ -64,3 +64,43 @@ SCENARIO("A callback is registered via 'onUpdate' to be called on property chang
 
   /************************************************************************************/
 }
+
+/**************************************************************************************/
+
+static bool switch_turned_on       = false;
+static bool switch_callback_called = false;
+
+void switch_callback()
+{
+  switch_turned_on       = false;
+  switch_callback_called = true;
+}
+
+SCENARIO("A (boolean) property is manipulated in the callback to its origin state", "[ArduinoCloudThing::decode]")
+{
+  GIVEN("CloudProtocol::V1")
+  {
+    ArduinoCloudThing thing(CloudProtocol::V1);
+    thing.begin();
+    encode(thing);
+
+    thing.addPropertyReal(switch_turned_on, "switch_turned_on", Permission::ReadWrite).onUpdate(switch_callback);
+
+    /* [{"n": "switch_turned_on", "vb": true}] = 81 A2 61 6E 70 73 77 69 74 63 68 5F 74 75 72 6E 65 64 5F 6F 6E 62 76 62 F5 */
+    uint8_t const payload[] = {0x81, 0xA2, 0x61, 0x6E, 0x70, 0x73, 0x77, 0x69, 0x74, 0x63, 0x68, 0x5F, 0x74, 0x75, 0x72, 0x6E, 0x65, 0x64, 0x5F, 0x6F, 0x6E, 0x62, 0x76, 0x62, 0xF5};
+    int const payload_length = sizeof(payload)/sizeof(uint8_t);
+    thing.decode(payload, payload_length);
+
+    REQUIRE(switch_callback_called == true);
+
+    /* Since the property was reset to its origin state in the callback we
+     * expect that on the next call to encode this change is propagated to
+     * the cloud.
+     */
+
+    /* [{"n": "switch_turned_on", "vb": false}] = 81 BF 61 6E 70 73 77 69 74 63 68 5F 74 75 72 6E 65 64 5F 6F 6E 62 76 62 F4 FF */
+    std::vector<uint8_t> const expected = {0x81, 0xBF, 0x61, 0x6E, 0x70, 0x73, 0x77, 0x69, 0x74, 0x63, 0x68, 0x5F, 0x74, 0x75, 0x72, 0x6E, 0x65, 0x64, 0x5F, 0x6F, 0x6E, 0x62, 0x76, 0x62, 0xF4, 0xFF};
+    std::vector<uint8_t> const actual = encode(thing);
+    REQUIRE(actual == expected);
+  }
+}
