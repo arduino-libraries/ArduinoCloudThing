bc5cd0f293ade91686c1c88160cbceaecaa559f3
ilcato <ilcato@yahoo.com>
2019-02-19 10:13:04 +0100

    definition of sychronization callback moved into ArduinoCloudThing, test cases completed

--- examples/thingProperties.h
+++ examples/thingProperties.h
@@ -4,22 +4,31 @@
 char ssid[] = SECRET_SSID;    // Network SSID (name)
 char pass[] = SECRET_PASS;    // Network password (use for WPA, or use as key for WEP)
 
-#define THING_ID "cfac3a86-8f3c-476f-b7e3-34d34c6f3dc6"
+#define THING_ID "PUT_YOUT_THING_ID_HERE"
 
 void onSwitchChange();
+<<<<<<< HEAD
 void onControllerChange();
 void onSwitchAutoSync(ArduinoCloudProperty<bool> property);
 void onSwitchForceCloudSync(ArduinoCloudProperty<bool> property);
 void onSwitchForceDeviceSync(ArduinoCloudProperty<bool> property);
 void onControllerSync(ArduinoCloudProperty<float> property);
+=======
+
+void onSwitchSync(ArduinoCloudProperty<T> property);
+>>>>>>> definition of sychronization callback moved into ArduinoCloudThing, test cases completed
 
 bool Switch;
 float controller;
 
 void initProperties(){
   ArduinoCloud.setThingId(THING_ID);
+<<<<<<< HEAD
   ArduinoCloud.addProperty(Switch, READWRITE, PROPERTIES_SYNC_AUTO, onSwitchAutoSync, ON_CHANGE, onSwitchChange);
   ArduinoCloud.addProperty(controller, READWRITE, PROPERTIES_SYNC_AUTO, onControllerSync, ON_CHANGE, onControllerChange);
+=======
+  ArduinoCloud.addProperty(Switch, READWRITE, ON_CHANGE, onSwitchChange, onSwitchSync);
+>>>>>>> definition of sychronization callback moved into ArduinoCloudThing, test cases completed
 }
 
 ConnectionManager *ArduinoIoTPreferredConnection = new WiFiConnectionManager(SECRET_SSID, SECRET_PASS);
